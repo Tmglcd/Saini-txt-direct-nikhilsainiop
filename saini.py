fe89e3d7a509654b8a24b4b881226247b2000f07
Tmglcd <tmglcd06@gmail.com>
2025-06-18 14:07:56 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -295,8 +295,8 @@ async def download_and_decrypt_video(url, cmd, name, key):
 async def send_vid(bot: Client, m: Message, cc, filename, thumb, name, prog, channel_id):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:10 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply1 = await bot.send_message(channel_id, f"<blockquote><b>Uploading Video:</b></blockquote>\n{name}")
-    reply = await m.reply_text(f"<blockquote><b>Generate Thumbnail:</b></blockquote>\n{name}")
+    reply1 = await bot.send_message(channel_id, f"**📩 Uploading Video 📩:-**\n<blockquote>**{name}**</blockquote>")
+    reply = await m.reply_text(f"**Generate Thumbnail:**\n<blockquote>**{name}**</blockquote>")
     try:
         if thumb == "/d":
             thumbnail = f"{filename}.jpg"
